2e1837b7297d0e302d067956e8c02335284ecde2
Екатерина <k-gaponova@mail.ru>
2018-12-16 18:18:05 +0300

    Изменение структуры входных данных для заполнения объявлений

--- js/map.js
+++ js/map.js
@@ -1,8 +1,27 @@
 'use strict';
 
-var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+// var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 
-var types = ['palace', 'flat', 'house', 'bungalo'];
+// var types = ['palace', 'flat', 'house', 'bungalo'];
+
+var types = {
+  flat: {
+    oneOfTypes: 'flat',
+    titles: ['Большая уютная квартира', 'Маленькая неуютная квартира']
+  },
+  palace: {
+    oneOfTypes: 'palace',
+    titles: ['Огромный прекрасный дворец', 'Маленький ужасный дворец']
+  },
+  house: {
+    oneOfTypes: 'house',
+    titles: ['Красивый гостевой домик', 'Некрасивый негостеприимный домик']
+  },
+  bungalo: {
+    oneOfTypes: 'bungalo',
+    titles: ['Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде']
+  }
+};
 
 var times = ['12:00', '13:00', '14:00'];
 
@@ -12,6 +31,10 @@ var arrayPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http:/
 
 var arrayAvatarNumbers = [];
 var arrayRandomTitles = [];
+var arrayRandomTitlesFlat = [];
+var arrayRandomTitlesPalace = [];
+var arrayRandomTitlesHouse = [];
+var arrayRandomTitlesBungalo = [];
 
 var getRandom = function (min, max) {
   return Math.floor(min + Math.random() * (max - min + 1));
@@ -63,14 +86,46 @@ var renderAd = function () {
   var localOffer = {};
 
   // "title": строка, заголовок предложения, одно из фиксированных значений, значения не повторяются
-  var indexTitle = getRandom(0, 7);
-  var flagTitle = findDuplicateValue(arrayRandomTitles, titles[indexTitle]);
+
+  var indexOfTypes;
+  var typeOfAccommodation;
+  var choiceOfAccommodationType = function () {
+    do {
+      indexOfTypes = getRandom(0, 3);
+      if (indexOfTypes === 0) {
+        typeOfAccommodation = types.flat;
+        arrayRandomTitles = arrayRandomTitlesFlat;
+      } else if (indexOfTypes === 1) {
+        typeOfAccommodation = types.palace;
+        arrayRandomTitles = arrayRandomTitlesPalace;
+      } else if (indexOfTypes === 2) {
+        typeOfAccommodation = types.house;
+        arrayRandomTitles = arrayRandomTitlesHouse;
+      } else if (indexOfTypes === 3) {
+        typeOfAccommodation = types.bungalo;
+        arrayRandomTitles = arrayRandomTitlesBungalo;
+      }
+    } while (arrayRandomTitles.length > 2);
+  };
+  choiceOfAccommodationType();
+  var indexTitle = getRandom(0, 1);
+  var flagTitle = findDuplicateValue(arrayRandomTitles, typeOfAccommodation.titles[indexTitle]);
   while (flagTitle) {
-    indexTitle = getRandom(0, 7);
-    flagTitle = findDuplicateValue(arrayRandomTitles, titles[indexTitle]);
+    choiceOfAccommodationType();
+    indexTitle = getRandom(0, 1);
+    flagTitle = findDuplicateValue(arrayRandomTitles, typeOfAccommodation.titles[indexTitle]);
   }
-  localOffer.title = titles[indexTitle];
-  arrayRandomTitles[arrayRandomTitles.length] = titles[indexTitle];
+  localOffer.title = typeOfAccommodation.titles[indexTitle];
+  arrayRandomTitles[arrayRandomTitles.length] = typeOfAccommodation.titles[indexTitle];
+
+  // var indexTitle = getRandom(0, );
+  // var flagTitle = findDuplicateValue(arrayRandomTitles, titles[indexTitle]);
+  // while (flagTitle) {
+  //   indexTitle = getRandom(0, 7);
+  //   flagTitle = findDuplicateValue(arrayRandomTitles, titles[indexTitle]);
+  // }
+  // localOffer.title = titles[indexTitle];
+  // arrayRandomTitles[arrayRandomTitles.length] = titles[indexTitle];
 
   // "address": строка, адрес предложения, представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
   localOffer.address = localLocation.x + ', ' + localLocation.y;
@@ -79,18 +134,20 @@ var renderAd = function () {
   localOffer.price = getRandom(1000, 1000000);
 
   // "type": строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
+  localOffer.type = typeOfAccommodation.oneOfTypes;
+
   // var indexType = getRandom(0, 3);
   // localOffer.type = types[indexType];
-  localOffer.type = '';
-  if (localOffer.title === 'Большая уютная квартира' || localOffer.title === 'Маленькая неуютная квартира') {
-    localOffer.type = types[1];
-  } else if (localOffer.title === 'Огромный прекрасный дворец' || localOffer.title === 'Маленький ужасный дворец') {
-    localOffer.type = types[0];
-  } else if (localOffer.title === 'Красивый гостевой домик' || localOffer.title === 'Некрасивый негостеприимный домик') {
-    localOffer.type = types[2];
-  } else if (localOffer.title === 'Уютное бунгало далеко от моря' || localOffer.title === 'Неуютное бунгало по колено в воде') {
-    localOffer.type = types[3];
-  }
+  // localOffer.type = '';
+  // if (localOffer.title === 'Большая уютная квартира' || localOffer.title === 'Маленькая неуютная квартира') {
+  //   localOffer.type = types[1];
+  // } else if (localOffer.title === 'Огромный прекрасный дворец' || localOffer.title === 'Маленький ужасный дворец') {
+  //   localOffer.type = types[0];
+  // } else if (localOffer.title === 'Красивый гостевой домик' || localOffer.title === 'Некрасивый негостеприимный домик') {
+  //   localOffer.type = types[2];
+  // } else if (localOffer.title === 'Уютное бунгало далеко от моря' || localOffer.title === 'Неуютное бунгало по колено в воде') {
+  //   localOffer.type = types[3];
+  // }
 
   // "rooms": число, случайное количество комнат от 1 до 5
   localOffer.rooms = getRandom(1, 5);
@@ -139,6 +196,11 @@ var Ads = [];
 for (var indexAd = 0; indexAd < numberOfAds; indexAd++) {
   Ads[indexAd] = renderAd();
 }
+
+// console.log(arrayRandomTitlesFlat);
+// console.log(arrayRandomTitlesPalace);
+// console.log(arrayRandomTitlesHouse);
+// console.log(arrayRandomTitlesBungalo);
 
 // создание меток на карте
 var map = document.querySelector('.map');
