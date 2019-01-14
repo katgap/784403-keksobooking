eb9a08c9d9ef3eb4486dead88ea5788bcb890183
Екатерина <k-gaponova@mail.ru>
2019-01-14 19:15:17 +0300

    Доработка активного состояния

--- js/map.js
+++ js/map.js
@@ -391,5 +391,51 @@ var switchToActiveState = function () {
 var mouseup = mapPinMain.addEventListener('click', function () {
   switchToActiveState();
   showAddress(initialX, initialY);
+  drawMark(Ads, listOfMarks);
 });
 
+// ___________________________________
+// Задание: "Личный проект: доверяй, но проверяй"
+
+//var selectedType = document.getElementById('type');
+
+// установка минимальной цены
+/*var chooseMinPrice = function () {
+  var selectedPrice = document.getElementById('price'); 
+  if (selectedType.value === 'bungalo') {
+    selectedPrice.min = 0;
+    selectedPrice.placeholder = '1000';
+  } else if (selectedType.value === 'flat') {
+    selectedPrice.min = 1000;
+    selectedPrice.placeholder = '5000';
+  } else if (selectedType.value === 'house') {
+    selectedPrice.min = 5000;
+    selectedPrice.placeholder = '10000';
+  } else if (selectedType.value === 'palace') {
+    selectedPrice.min = 10000;
+    selectedPrice.placeholder = '15000';
+  } 
+}
+
+chooseMinPrice();
+selectedType.addEventListener('change', function () {
+  chooseMinPrice();
+});
+
+// установка времени въезда и выезда
+var timeIn = document.getElementById('timein');
+var timeOut = document.getElementById('timeout');
+
+var chooseTimeIn = function () {
+  timeOut.value = timeIn.value;
+}
+var chooseTimeOut = function () {
+  timeIn.value = timeOut.value;
+}
+
+timeIn.addEventListener('change', function () {
+  chooseTimeIn();
+});
+timeOut.addEventListener('change', function () {
+  chooseTimeOut();
+});*/
